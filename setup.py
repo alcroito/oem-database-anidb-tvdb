5fb018f2817fcd1fd5b82d7ff938b0602cd6d5b6
Alexandru Croitor <placinta@gmail.com>
2020-11-02 00:47:15 +0100

    Bumped version to 1.19.20

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ PACKAGE_NAME = 'oem-database-anidb-tvdb'
 def build_config():
     config = {
         'name': PACKAGE_NAME,
-        'version': '1.19.19',
+        'version': '1.19.20',
 
         'author': 'Dean Gardiner',
         'author_email': 'me@dgardiner.net',
